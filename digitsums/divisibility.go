8c982a13062c627112d68a5a8b9a5abaa044ae27
SebastianLorenz <sebastian.lorenz.1@gmx.de>
2026-01-16 09:27:32 +0100

    Divis3

--- digitsums/divisibility.go
+++ digitsums/divisibility.go
@@ -1,9 +1,27 @@
 package digitsums
 
+import "math"
+
 // IsDivisibleBy3 prüft, ob die gegebene Zahl n durch 3 teilbar ist.
 //
 // Randbedingung: Verwenden Sie NICHT den Modulo-Operator (%).
 func IsDivisibleBy3(n int) bool {
-	// TODO
-	return false
+	m := float64(n) / float64(3)
+
+	if m > 0 {
+		if m == math.Floor(m) {
+			return true
+		} else {
+			return false
+		}
+	}
+	if m < 0 {
+		if m == math.Ceil(m) {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return true
+	}
 }
